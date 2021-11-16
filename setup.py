516839f9a41cbb70b8e0fa865f60cad4ce1234db
Lokesh Raj <32189830+aQwus@users.noreply.github.com>
2021-11-16 18:36:25 +0530

    changed setup name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bar_chart_race",
+    name="bar_chart_race2",
     version=version,
     author="Ted Petrou",
     author_email="petrou.theodore@gmail.com",
