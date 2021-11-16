83d3a441c3b7e3be160a53970f84d11099b2d2bc
Lokesh Raj <32189830+aQwus@users.noreply.github.com>
2021-11-16 16:16:47 +0530

    corrected the setup name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bar_chart_race1",
+    name="bar_chart_race",
     version=version,
     author="Ted Petrou",
     author_email="petrou.theodore@gmail.com",
