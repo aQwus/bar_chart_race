9b6d71739dbc8ee76475be95e95ef7ce319b99ee
Lokesh Raj <32189830+aQwus@users.noreply.github.com>
2021-11-16 19:08:13 +0530

    changed image zoom level

--- bar_chart_race/_bar_chart_race.py
+++ bar_chart_race/_bar_chart_race.py
@@ -205,7 +205,7 @@ class _BarChartRace(CommonChart):
         #load image as an OffsetImage object
         img_name      = get_image_name(name, self.img_name_dict)
         img           = get_image_label(self.img_label_folder,img_name)
-        im            = OffsetImage(img,zoom=.1) # change zoom value based on icon image's size
+        im            = OffsetImage(img,zoom=.25) # change zoom value based on icon image's size
         im.image.axes = ax 
 
         
@@ -251,7 +251,7 @@ class _BarChartRace(CommonChart):
         #load image as an OffsetImage object
         img_name      = get_image_name(name, self.img_name_dict)
         img           = get_image_label(self.img_label_folder,img_name)
-        im            = OffsetImage(img,zoom=.1) # change zoom value based on icon image's size
+        im            = OffsetImage(img,zoom=.25) # change zoom value based on icon image's size
         im.image.axes = ax 
 
         #renderer = self.fig.canvas.renderer
